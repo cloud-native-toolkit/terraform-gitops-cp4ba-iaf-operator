41f07e3fa58a22d1041c9334d184876fe44e9ed4
Dinesh C Pandey <dineshchandrapandey@in.ibm.com>
2022-02-25 12:47:41 +0530

    Added  namespace
    Signed-off-by: Dinesh C Pandey <dineshchandrapandey@in.ibm.com>

--- variables.tf
+++ variables.tf
@@ -66,7 +66,7 @@ variable "git_credentials" {
 variable "namespace" {
   type        = string
   default = "cp4ba"
-  description = "The namespace where the application should be deployed"
+  description = "The namespace where the application should be deployed" 
 }
 
 variable "kubeseal_cert" {
